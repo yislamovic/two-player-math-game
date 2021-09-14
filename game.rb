d6adc884d37ba69ebe54012ad3e0a1024a4987f3
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-13 21:29:37 -0400

    complete game

--- game.rb
+++ game.rb
@@ -19,21 +19,52 @@ class Game
     game_start()
   end
 
-  def minus_life
-    @@player1.lives -= 1
-  end
-
-  def generate_question()
+  def generate_question(name)
     a = Random.new.rand(1..20)
     b = Random.new.rand(1..20)
-    puts "What is #{a} + #{b}?"
+    puts "Player #{name}, What is #{a} + #{b}?"
     answer = a + b
   end  
 
-  def game_start
-    while @@player1.lives >= 1 && @@player2.lives >= 1
-      generate_question()
-      minus_life()
+  def player_turn1
+    answer = generate_question(@@player1.name)
+    input = Game_IO.new()
+    input.get_input()
+    if answer.to_s === input.input.to_s
+      puts "Correct" 
+    else
+      puts "Incorrect"
+      @@player1.lives -= 1
+      puts "Player #{@@player1.name} has #{@@player1.lives} lives left"
     end  
+    if @@player1.lives >= 1
+      puts "----------New Turn----------"
+      player_turn2()
+    else
+      puts "The winner is #{@@player2.name}"
+    end  
+  end  
+
+  def player_turn2
+    answer = generate_question(@@player2.name)
+    input = Game_IO.new()
+    input.get_input()
+    if answer.to_s === input.input.to_s
+      puts "Correct"  
+    else
+      puts "Incorrect"
+      @@player2.lives -= 1
+      puts "Player #{@@player2.name} has #{@@player2.lives} lives left"
+    end  
+    if @@player1.lives >= 1 && @@player2.lives >= 1
+      puts "----------New Turn----------"
+      player_turn1()
+    else
+      puts "The winner is #{@@player1.name}"
+    end  
+  end
+
+  def game_start
+    player_turn1()
   end
 end
